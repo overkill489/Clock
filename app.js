6aad08136c68326defae0f2f05824278ec294553
overkill489 <overkill.alt.489@gmail.com>
2026-08-23 17:57:31 -0500

    08/23/2026 @5:57pm

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const secondHandle = document.querySelector(".handle__second");
-const minuteHandle = document.querySelector(".handle__minute");
-const hourHandle = document.querySelector(".handle__hour");
+const hourHandle = document.querySelector(`.handle__hour`);
+const minuteHandle = document.querySelector(`.handle__minute`);
+const secondHandle = document.querySelector(`.handle__second`);
+const millisecondHandle = document.querySelector(`.handle__milliSecond`);
 
-const d = new Date();
-const hours = d.getHours();
-const minutes = d.getMinutes();
-const second = d.getSeconds();
+requestAnimationFrame(setHandles);
 
-console.log(hours * 30);
+function setHandles() {
+  const d = new Date();
 
-hourHandle.style.transform = `transform: rotate(${hours * 30})`;
+  const hours = d.getHours();
+  const minutes = d.getMinutes();
+  const seconds = d.getSeconds();
+  const milliseconds = d.getMilliseconds();
+
+  const extraHoursAngle = minutes * 0.5;
+  const hoursAngle = hours * 30 + extraHoursAngle;
+
+  const extraMinutesAngle = seconds * 0.1;
+  const minutesAngle = minutes * 6 + extraMinutesAngle;
+
+  const secondDegrees = (seconds + milliseconds / 1000) * 6;
+
+  hourHandle.style.transform = `translateX(-50%) rotate(${hoursAngle}deg)`;
+  minuteHandle.style.transform = `translateX(-50%) rotate(${minutesAngle}deg)`;
+  secondHandle.style.transform = `translateX(-50%) rotate(${secondDegrees}deg)`;
+  millisecondHandle.style.transform = `translateX(-50%) rotate(${milliseconds}deg)`;
+  requestAnimationFrame(setHandles);
+}
